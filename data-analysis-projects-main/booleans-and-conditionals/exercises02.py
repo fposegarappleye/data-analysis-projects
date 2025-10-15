ffd685222df46f5b18f29533afd34465a3cc144e
Francis Posega Rappleye <fposegarappleye@gmail.com>
2025-10-14 20:22:16 -0500

    Completed second studio

---
+++ data-analysis-projects-main/booleans-and-conditionals/exercises02.py
@@ -0,0 +1,21 @@
+engine_indicator_light = "red blinking"
+space_suits_on = True
+shuttle_cabin_ready = True
+crew_status = space_suits_on and shuttle_cabin_ready
+computer_status_code = 200
+shuttle_speed = 15000
+
+# 3) Write conditional expressions to satisfy the following safety rules:
+
+# a) If crew_status is true, print "Crew Ready" else print "Crew Not Ready".
+
+
+# b) If computer_status_code is 200, print "Please stand by. Computer is rebooting." Else if computer_status_code is 400, print "Success! Computer online." Else print "ALERT: Computer offline!"
+
+
+# c) If shuttle_speed is > 17,500, print "ALERT: Escape velocity reached!" Else if shuttle_speed is < 8000, print "ALERT: Cannot maintain orbit!" Else print "Stable speed".
+
+
+# 4) PREDICT: Do the code blocks shown in the Section D produce the same result?
+
+# print("Yes" or "No")
