d8e7971b0a93ccd6331f2d9e0957a288e4546f49
Francis Posega Rappleye <fposegarappleye@gmail.com>
2025-11-19 18:29:26 -0600

    Completed Part 4 Studio

--- sql-part-4/exercises/sql-part-4-exercises.sql
+++ sql-part-4/exercises/sql-part-4-exercises.sql
@@ -40,10 +40,158 @@
 -- SELECT book_id, language_code FROM BooksDB.dbo.books
 -- WHERE language_code = 'en-GB'
 
-SELECT tag_id, tag_name FROM BooksDB.dbo.tags
-WHERE tag_id IN (
-    SELECT tag_id
-    FROM BooksDB.dbo.book_tags
-    GROUP BY tag_id
-    HAVING SUM(count) > 100000
+-- SELECT tag_id, tag_name FROM BooksDB.dbo.tags
+-- WHERE tag_id IN (
+--     SELECT tag_id
+--     FROM BooksDB.dbo.book_tags
+--     GROUP BY tag_id
+--     HAVING SUM(count) > 100000
+-- )
+
+-- Referencing the Books db
+-- SELECT title FROM BooksDB.dbo.books
+-- WHERE authors LIKE '%,%'
+
+-- SELECT title FROM BooksDB.dbo.books
+-- WHERE book_id IN (
+--     SELECT goodreads_book_id FROM BooksDB.dbo.book_tags
+--     WHERE tag_id = (
+--         SELECT tag_id FROM BooksDB.dbo.tags
+--         WHERE tags.tag_name = 'Meditation'
+--     )
+-- )
+
+
+
+
+
+-- Average rating of 4.2 or higher
+-- Descending order by rating
+
+-- SELECT title
+-- FROM BooksDB.dbo.books 
+-- WHERE title IN (
+--     SELECT original_title
+--     FROM (
+--         SELECT original_title FROM BooksDB.dbo.books
+--         WHERE original_title IS NOT NULL
+--     )
+--     WHERE average_rating > 4.2
+-- )
+
+-- (
+--     SELECT title, average_rating
+--     FROM BooksDB.dbo.books 
+--     WHERE title IN (
+--         SELECT original_title FROM BooksDB.dbo.books
+--         WHERE average_rating > 4.2
+--     )
+--     EXCEPT
+
+--     SELECT title, average_rating
+--     FROM BooksDB.dbo.books WHERE original_title IS NULL
+-- )
+-- ORDER BY average_rating DESC
+
+
+-- SELECT title,average_rating FROM BooksDB.dbo.books
+-- WHERE average_rating > 4.2 (
+--     SELECT book_id FROM BooksDB.dbo.books 
+--     WHERE title = original_title
+    
+--     EXCEPT
+
+--     SELECT book_id
+--     FROM BooksDB.dbo.books
+--     WHERE original_title IS NULL
+-- )
+-- ORDER BY average_rating DESC
+
+
+-- SELECT title, average_rating
+-- FROM BooksDB.dbo.books 
+-- WHERE book_id IN (
+--     SELECT book_id 
+--     FROM BooksDB.dbo.books
+--     WHERE average_rating >= 4.2
+-- )
+
+-- EXCEPT
+
+-- SELECT title, average_rating
+-- FROM BooksDB.dbo.books
+-- WHERE original_title IS NULL
+
+-- ORDER BY average_rating DESC
+
+-- SELECT title FROM BooksDB.dbo.books
+-- WHERE book_id IN (
+--     SELECT goodreads_book_id FROM BooksDB.dbo.book_tags
+--     WHERE tag_id = (
+--         SELECT tag_id FROM BooksDB.dbo.tags
+--         WHERE tags.tag_name = 'Meditation'
+--     )
+-- )
+
+
+
+
+
+-- SELECT original_publication_year, average_rating
+-- WHERE book_id IN (
+--     SELECT AVG()
+-- )
+-- FROM BooksDB.dbo.books
+
+-- SELECT book_id
+-- FROM BooksDB.dbo.ratings
+-- WHERE AVG(rating*1.0) > (
+    
+-- )
+
+
+-- SELECT book_id,  FROM BooksDB.dbo.ratings
+-- GROUP BY book_id
+
+-- Compare publication time with average reader ratings
+-- SELECT book_id, AVG(rating*1.0) FROM BooksDB.dbo.ratings
+-- GROUP BY book_id
+-- HAVING AVG(rating*1.0) > (
+--     SELECT average_rating
+--     FROM BooksDB.dbo.books
+-- )
+-- -- 
+
+
+
+
+
+
+-- SELECT book_id, AVG(rating)
+-- FROM BooksDB.dbo.ratings
+-- GROUP BY book_id
+-- HAVING AVG(rating*1.0) > (
+--     SELECT average_rating 
+--     FROM BooksDB.dbo.books
+--     WHERE books.book_id = book_id
+-- )
+
+-- Select Publication time
+SELECT 
+    title AS "Title",
+    original_publication_year AS "Year",
+    average_rating AS "Original Reviews", 
+    (
+        SELECT CAST(AVG(rating*1.0) AS DECIMAL(3,2))
+        FROM BooksDB.dbo.ratings AS r
+        WHERE b.book_id = r.book_id
+        GROUP BY book_id
+    ) AS "Reader Reviews"
+FROM BooksDB.dbo.books AS b
+WHERE average_rating < (
+    SELECT AVG(rating*1.0) AS 'avg'
+    FROM BooksDB.dbo.ratings AS r
+    WHERE b.book_id = r.book_id
+    GROUP BY book_id
 )
+ORDER BY original_publication_year, title
